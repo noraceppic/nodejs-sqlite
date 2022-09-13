3e44a90892b4934f9e97897b41baa40b3f3ad799
noraceppic <messanora86@gmail.com>
2022-09-13 16:43:08 +0200

    afficher la date de creation dun  champs dan sune bdd

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ let db = new sqlite3.Database(dbname, (err)=>{
 });
 
 
-db.run ('CREATE TABLE IF NOT EXISTS planetes (name varchar(25))');
+//db.run ('CREATE TABLE IF NOT EXISTS planetes (id INTEGER PRIMARY KEY AUTOINCREMENT,name varchar(25), size VARCHAR(255) ,created_at DATETIME DEFAULT CURRENT_TIMSTAMP NOT NULL)');
  
-db.run ('INSERT INTO planetes (name) values(?)', ["Mars"]);
-db.run ('INSERT INTO planetes (name) values(?)', ["Terre"]);
-db.run ('INSERT INTO planetes (name) values(?)', ["Pluton"]);
-db.run ('INSERT INTO planetes (name) values(?)', ["jupiter"]);
-db.run ('INSERT INTO planetes (name) values(?)', ["Mercure"]); 
+db.run ('INSERT INTO planetes (name,size) values(?,?)', ["Mars", "33445352"]);
+db.run ('INSERT INTO planetes (name,size) values(?,?)', ["Terre", "25351542"]);
+db.run ('INSERT INTO planetes (name,size) values(?,?)', ["Pluton", "2152545"]);
+db.run ('INSERT INTO planetes (name,size) values(?,?)', ["jupiter", "2125"]);
+db.run ('INSERT INTO planetes (name,size) values(?,?)', ["Mercure", "25526125"]);  
 
-db.all ("SELECT * FROM planetes", (err, data) =>{
+//db.run (`DELETE FROM planetes  WHERE rowid = 2`);
+
+db.all ("SELECT rowid, * FROM planetes", (err, data) =>{
     if (err) throw err;
     console.log(data)
 });
